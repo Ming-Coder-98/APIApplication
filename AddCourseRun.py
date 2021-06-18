d02cd7d3ef3f23d891821ed6c90304900b0c2849
Ming <chongzheming98@gmail.com>
2021-06-18 15:17:31 +0800

    addcourserun form

--- AddCourseRun.py
+++ AddCourseRun.py
@@ -21,6 +21,126 @@ import pandas as pd
 
 from tooltip import CreateToolTip
 
+class addCourseRunPageOptional(tk.Frame):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+
+        load = Image.open("SKFBGPage.JPG")
+        render = ImageTk.PhotoImage(load)
+
+        # labels can be text or images
+        img2 = Label(self, image=render)
+        img2.image = render
+        img2.place(x=0, y=0, relwidth=1, relheight=1)
+
+        label_0 = Label(self, text="Optional fields", width=20, font=("bold", 20))
+        label_0.place(x=90, y=53)
+
+        label_1 = Label(self, text="Sessions", width=20, font=("bold", 15))
+        label_1.place(x=137, y=100)
+
+        label_2 = Label(self, text="Course Session Start Date:", width=20, font=("bold", 10))
+        label_2.place(x=80, y=135)
+
+        label_2_ttp = CreateToolTip(label_2,
+        'Start Date (YYYYMMDD) can be used as a parameter in the POST Request payload \n'
+        'Example: 20210616')
+
+        entry_2 = Entry(self)
+        entry_2.place(x=250, y=135)
+
+        label_3 = Label(self, text="Course Session Start Time:", width=20, font=("bold", 10))
+        label_3.place(x=80, y=160)
+
+        label_3_ttp = CreateToolTip(label_3,'Start time (HH:MM) can be used as a parameter in the POST Request payload \n'
+                                    'Example: 17:30')
+
+        entry_3 = Entry(self)
+        entry_3.place(x=250, y=160)
+
+        label_4 = Label(self, text="Course Session End Date:", width=20, font=("bold", 10))
+        label_4.place(x=80, y=185)
+
+        label_4_ttp = CreateToolTip(label_4,'End Date (YYYYMMDD) can be used as a parameter in the POST Request payload \n'
+                                    'Example: 20210621')
+
+        entry_4 = Entry(self)
+        entry_4.place(x=250, y=185)
+
+        label_5 = Label(self, text="Course Session End Time:", width=20, font=("bold", 10))
+        label_5.place(x=80, y=210)
+
+        label_5_ttp = CreateToolTip(label_5, \
+                                    'End time (HH:MM) can be used as a parameter in the POST Request payload \n'
+                                    'Example: 18:30')
+
+        entry_5 = Entry(self)
+        entry_5.place(x=250, y=210)
+
+        label_6 = Label(self, text="Mode of Training:", width=20, font=("bold", 10))
+        label_6.place(x=80, y=235)
+
+        label_6_ttp = CreateToolTip(label_6,
+                                    'Mode of Training is used as a parameter in the POST Request payload \n'
+                                    'Example: 9')
+
+        entry_6 = Entry(self)
+        entry_6.place(x=250, y=235)
+
+        label_7 = Label(self, text="Trainers", width=20, font=("bold", 15))
+        label_7.place(x=137, y=270)
+
+        label_8 = Label(self, text="Trainer name:", width=20, font=("bold", 10))
+        label_8.place(x=80, y=305)
+
+        label_8_ttp = CreateToolTip(label_8, \
+                                    'Name of the trainer can be used as a parameter in the POST Request payload \n'
+                                    'Example: John')
+
+        entry_8 = Entry(self)
+        entry_8.place(x=250, y=305)
+
+        label_9 = Label(self, text="Trainer Email:", width=20, font=("bold", 10))
+        label_9.place(x=80, y=330)
+
+        label_9_ttp = CreateToolTip(label_9, \
+                                    'Email of trainner canbe used as a parameter in the POST Request payload \n'
+                                  'Example: tester@gmail.com')
+
+        entry_9 = Entry(self)
+        entry_9.place(x=250, y=330)
+
+        label_10 = Label(self, text="Trainer Code:", width=20, font=("bold", 10))
+        label_10.place(x=80, y=355)
+
+        label_10_ttp = CreateToolTip(label_10, \
+                                     'Code of Trainer can be used as a parameter in the POST Request payload \n'
+                                     '1 - Existing, 2 - New \n'
+                                     'Example: 1')
+
+        entry_10 = Entry(self)
+        entry_10.place(x=250, y=355)
+
+        label_11 = Label(self, text="Trainer description:", width=20, font=("bold", 10))
+        label_11.place(x=80, y=380)
+
+        label_11_ttp = CreateToolTip(label_11, \
+                                     'Simple description of trainer can be used as a parameter in the POST Request payload \n'
+                                     'Example: New Trainer')
+
+        entry_11 = Entry(self)
+        entry_11.place(x=250, y=380)
+
+        backButton = tk.Button(self, text="Back", bg="white", width=10, pady=5,
+                               command=lambda: controller.show_frame(addCourseRunPageSelect)
+                               )
+        backButton.place(relx=0.055, rely=0.021, anchor=CENTER)
+
+
+        previewButton = tk.Button(self, text="Next", bg="white", width=25, pady=5, command=lambda: controller.show_frame(addCourseRunPageForm))
+        previewButton.place(x=250, y=440, anchor=CENTER)
+
+
 class addCourseRunPageSelect(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
@@ -130,17 +250,70 @@ class addCourseRunPageSelect(tk.Frame):
         label_9.place(x=80, y=420)
 
         label_9_ttp = CreateToolTip(label_9, \
-        'Mode of Training (Integer) is used as a parameter in the POST Request payload \n'
+        'Mode of Training is used as a parameter in the POST Request payload \n'
         'Example: 9')
 
         entry_9 = Entry(self)
         entry_9.place(x=250, y=420)
 
-        previewButton = tk.Button(self, text="Next", bg="white", width=25, pady=5, command=lambda: controller.show_frame(addCourseRunPageForm) if self.var.get() == 2 else controller.show_frame(addCourseRunPageFormFileUpload) )
+        label_10 = Label(self, text="Course Run Room:", width=20, font=("bold", 10))
+        label_10.place(x=80, y=445)
+
+        label_10_ttp = CreateToolTip(label_10, \
+        'Course Run Room Number  is used as a parameter in the POST Request payload \n'
+        'Example: 9')
+
+        entry_10 = Entry(self)
+        entry_10.place(x=250, y=445)
+
+        label_11 = Label(self, text="Course Run Room Floor:", width=20, font=("bold", 10))
+        label_11.place(x=80, y=470)
+
+        label_11_ttp = CreateToolTip(label_11, \
+        'The level of the Course Run room is used as a parameter in the POST Request payload \n'
+        'Example: 9')
+
+        entry_11 = Entry(self)
+        entry_11.place(x=250, y=470)
+
+        label_12 = Label(self, text="Course run Room Unit:", width=20, font=("bold", 10))
+        label_12.place(x=80, y=495)
+
+        label_12_ttp = CreateToolTip(label_12, \
+        'The unit of the Course Run room is used as a parameter in the POST Request payload \n'
+        'Example: 12')
+
+        entry_12 = Entry(self)
+        entry_12.place(x=250, y=495)
+
+        label_13 = Label(self, text="Course run Postal Code:", width=20, font=("bold", 10))
+        label_13.place(x=80, y=495)
+
+        label_13_ttp = CreateToolTip(label_13, \
+        'The postal code of the Course Run Venue is used as a parameter in the POST Request payload \n'
+        'Example: 12345')
+
+        entry_13 = Entry(self)
+        entry_13.place(x=250, y=495)
+
+        previewButton = tk.Button(self, text="Next", bg="white", width=25, pady=5, command=lambda: controller.show_frame(addCourseRunPageOptional) if self.var.get() == 2 else controller.show_frame(addCourseRunPageFormFileUpload) )
         previewButton.place(x=250, y=540, anchor=CENTER)
 
-        backButton = tk.Button(self, text="Back", bg="white", width=25, pady=5)
-        backButton.place(x=250, y=580, anchor=CENTER)
+        # This method is used to update the display information dynamically in "Payload" Tab whenever user key in a value
+
+        '''
+        def typing(event):
+            storeAndsave_all()
+
+        entry_1.bind('<KeyRelease>', typing)
+        entry_2.bind('<KeyRelease>', typing)
+        entry_3.bind('<KeyRelease>', typing)
+        entry_4.bind('<KeyRelease>', typing)
+        entry_5.bind('<KeyRelease>', typing)
+        entry_6.bind('<KeyRelease>', typing)
+        entry_7.bind('<KeyRelease>', typing)
+        entry_8.bind('<KeyRelease>', typing)
+        entry_9.bind('<KeyRelease>', typing)
 
         def storeAndsave_all():
             # load config File
@@ -148,16 +321,24 @@ class addCourseRunPageSelect(tk.Frame):
             configInfoJson = json.loads(configInfo)
 
             configInfoJson["course"]["courseReferenceNumber"] = entry_1.get()
-            configInfoJson["course"]["runs"]["courseAdminEmail"] = entry_2.get()
-            configInfoJson["course"]["runs"]["registrationDates"]["opening"] = entry_3.get()
-            configInfoJson["course"]["runs"]["registrationDates"]["closing"] = entry_4.get()
-            configInfoJson["course"]["runs"]["courseDates"]["start"] = entry_5.get()
-            configInfoJson["course"]["runs"]["courseDates"]["end"] = entry_6.get()
-            configInfoJson["course"]["runs"]["courseVacancy"]["code"] = entry_7.get()
-            configInfoJson["course"]["runs"]["courseVacancy"]["description"] = entry_8.get()
-            configInfoJson["course"]["runs"]["modeOfTraining"] = entry_9.get()
+            configInfoJson["course"]["runs"][0]["courseAdminEmail"] = entry_2.get()
+            configInfoJson["course"]["runs"][0]["registrationDates"]["opening"] = int(entry_3.get())
+            configInfoJson["course"]["runs"][0]["registrationDates"]["closing"] = int(entry_4.get())
+            configInfoJson["course"]["runs"][0]["courseDates"]["start"] = int(entry_5.get())
+            configInfoJson["course"]["runs"][0]["courseDates"]["end"] = int(entry_6.get())
+            configInfoJson["course"]["runs"][0]["courseVacancy"]["code"] = entry_7.get()
+            configInfoJson["course"]["runs"][0]["courseVacancy"]["description"] = entry_8.get()
+            configInfoJson["course"]["runs"][0]["modeOfTraining"] = entry_9.get()
+            configInfoJson["course"]["runs"][0]["venue"]["room"] = entry_10.get()
+            configInfoJson["course"]["runs"][0]["venue"]["floor"] = entry_11.get()
+            configInfoJson["course"]["runs"][0]["venue"]["unit"] = entry_12.get()
+            configInfoJson["course"]["runs"][0]["venue"]["postalCode"] = entry_13.get()
+
             # Save config File
             saveJsonFormat(configInfoJson, "EmptyCourseRunPayLoad.json")
+            '''
+
+
 
     def show_frame(self, cont):
         frame = self.frames[cont]
@@ -211,8 +392,15 @@ class addCourseRunPageForm(tk.Frame):
         responseText.place(height=405, width=440, y=20)
         responseText.bind("<Key>", lambda e: "break")
 
+        def submitCallBack():
+            addCourseRunInfo = loadFile("EmptyCourseRunPayLoad.json")
+            addCourseRunInfoJson = json.loads(addCourseRunInfo)
+            print(addCourseRunInfoJson)
+            resp = createCourserun(json.dumps(addCourseRunInfoJson))
+            print(resp.text)
+
         createButton = tk.Button(self, text="Create", bg="white", width=25, pady=5,
-                                )
+                                command = lambda: submitCallBack())
         createButton.place(relx=0.5, rely=0.22, anchor=CENTER)
         backButton = tk.Button(self, text="Back", bg="white", width=10, pady=5,
                                command=lambda: controller.show_frame(addCourseRunPageSelect)
@@ -291,6 +479,7 @@ class addCourseRunPageForm(tk.Frame):
         # This method activates two other methods.
         # 1) this method calls the delete method in courseRunFunction and return the response
         # 2) Based on the response, if a status 200 is received, it will display the response
+
         def deleteCallBack(runId):
             resp = deleteCourserun(runId)
             if (resp.status_code < 400):
--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from AssessmentFunction import addAssessment
 from EnrolmentFunction import addEnrolment, enrollmentInitialization
 from AttendanceFunction import uploadAttendance
 from courseRunFunctions import curlGetRequestViewCourseRun, curlPostRequest, deleteCourserun, getCourseRun, getDeleteCourseRunPayLoad
-from AddCourseRun import addCourseRunPageForm, addCourseRunPageFormFileUpload, addCourseRunPageSelect
+from AddCourseRun import addCourseRunPageForm, addCourseRunPageFormFileUpload, addCourseRunPageSelect, addCourseRunPageOptional
 
 from HttpRequestFunction import getHttpRequest, loadFile, saveJsonFormat
 import tkinter as tk
@@ -95,7 +95,7 @@ class APIProject(tk.Tk):
         
 
         self.frames = {}
-        for F in (updateCourseRunPageForm,updateCourseRunPageFormFileUpload,updateCourseRunPageSelect, addCourseRunPageFormFileUpload,addCourseRunPageForm, addCourseRunPageSelect, viewCourseRunPage, deleteCourseRunPage, StartPage):
+        for F in (updateCourseRunPageForm,updateCourseRunPageFormFileUpload,updateCourseRunPageSelect,addCourseRunPageOptional, addCourseRunPageFormFileUpload,addCourseRunPageForm, addCourseRunPageSelect, viewCourseRunPage, deleteCourseRunPage, StartPage):
             frame = F(container, self)
 
             self.frames[F] = frame
